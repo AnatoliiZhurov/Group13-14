5f6e99b2ad3bb3a945413b46833b67152b6926b4
AnatoliiZhurov <129226759+AnatoliiZhurov@users.noreply.github.com>
2023-06-02 02:24:00 +0300

    123

--- src/js/slider.js
+++ src/js/slider.js
@@ -3,6 +3,9 @@ const swiper = new Swiper('.swiper', {
   //   direction: 'vertical',
   direction: 'horizontal',
   loop: true,
+  slidesPerView: 'auto',
+  spaceBetween: 18,
+  slidesPerGroup: 1,
 
   // If we need pagination
   pagination: {
